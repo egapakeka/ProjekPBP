eeeb1b004eed6dd3ab0f24c998985989cae0194c
feliseren <feliseren05@gmail.com>
2025-09-22 09:12:07 +0700

    bebas lah

---
+++ app/Models/Carts.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carts extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id'];
+
+    public $timestamps = false;
+
+    public function users()
+    {
+        return $this->belongsTo(Users::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Cart_Items::class);
+    }
+}
