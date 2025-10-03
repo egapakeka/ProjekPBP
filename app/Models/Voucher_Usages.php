bda772316ad032922c3f7b1d5b307f8ab4f37f84
faraulia <frhkzara@gmail.com>
2025-10-03 11:32:04 +0700

    Update Voucher_Usages.php

--- app/Models/Voucher_Usages.php
+++ app/Models/Voucher_Usages.php
@@ -23,13 +23,10 @@ class Voucher_Usages extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
-<<<<<<<<< Temporary merge branch 1
     }
 
     public function order()
     {
         return $this->belongsTo(Orders::class);
-=========
->>>>>>>>> Temporary merge branch 2
     }
 }
