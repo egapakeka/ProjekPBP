be33d54aadc3cd701731a0bd4e540ccc51d0cf33
Nabes27 <nabihafarrel@gmail.com>
2025-10-09 10:43:04 +0700

    save

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -57,6 +57,9 @@
                             {{ __('Voucher') }}
                         </x-nav-link>
 
+                        <x-nav-link :href="route('admin.profile.edit')" :active="request()->routeIs('admin.profile.*')">
+                            {{ __('Profile Admin') }}
+                        </x-nav-link>
                     @endauth
                 </div>
             </div>
@@ -143,6 +146,10 @@
 
                 <x-responsive-nav-link :href="route('admin.vouchers.index')" :active="request()->routeIs('admin.vouchers.*')">
                     {{ __('Voucher') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('admin.profile.edit')" :active="request()->routeIs('admin.profile.*')">
+                    {{ __('Profile Admin') }}
                 </x-responsive-nav-link>
             @endauth
         </div>
