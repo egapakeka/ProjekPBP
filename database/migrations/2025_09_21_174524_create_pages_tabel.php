0fd88849c4f214e8e1a342c3adeedb34069a6b24
egapakeka <egaaryabima1@gmail.com>
2025-10-17 20:19:08 +0700

    migration fix
    update timestamp

--- database/migrations/2025_09_21_174524_create_pages_tabel.php
+++ database/migrations/2025_09_21_174524_create_pages_tabel.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->longText('content');
-            $table->timestamp('created_at')->useCurrent();
             $table->timestamps();
         });
     }
